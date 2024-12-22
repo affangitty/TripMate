b29919c51d3cd7a55a050abf24fca0b2f5d20a78
Affan Ahammed <116718291+affangitty@users.noreply.github.com>
2024-12-22 19:11:32 +0530

    map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import googleMapReact from 'google-map-react'
+import GoogleMapReact from 'google-map-react'
 import { Paper } from '@mui/material'
 import {Typography} from '@mui/material'
 import {useMediaQuery} from '@mui/material'
@@ -8,9 +8,22 @@ import {Rating} from '@mui/material'
 import useStyles from "./styles"
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+  const coordinates = { lat:0, lng:0 };
   return (
-    <div>
-      
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyB5Jzzs7Qq17f9hh_2xo9gfQx0_Tb651wQ' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50,50,50,50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+      </GoogleMapReact>
     </div>
   )
 }
